ad83b7f923eb9305214fb230733584ebcc59eb2c
Jasper Chen <jasperdyb@gmail.com>
2020-03-27 14:41:35 +0800

    Set up session/passport , build login function

--- app.js
+++ app.js
@@ -2,12 +2,27 @@
 
 const express = require('express')
 const app = express()
-
+const session = require('express-session')
+const passport = require('passport')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const port = 3000
+
+app.use(session({
+  secret: 'your secret key',
+  resave: 'false',
+  saveUninitialized: 'false',
+}))
+// 使用 Passport - 要在「使用路由器」前面
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
