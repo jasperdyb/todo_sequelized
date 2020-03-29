19375df950525475831c286196863fb09831c2a9
Jasper Chen <jasperdyb@gmail.com>
2020-03-29 11:35:56 +0800

    Add bcrypt function

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
 // 載入 user model
 const db = require('../models')
 const User = db.User
+
 // 登入頁面
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
 // 登入檢查
 router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
@@ -15,10 +19,12 @@ router.post('/login', (req, res, next) => {
     failureRedirect: '/users/login',
   })(req, res, next)
 })
+
 // 註冊頁面
 router.get('/register', (req, res) => {
   res.render('register')
 })
+
 // 註冊檢查
 router.post('/register', (req, res) => {
   const { name, email, password, password2 } = req.body
@@ -37,15 +43,24 @@ router.post('/register', (req, res) => {
         email,
         password,
       })
-      newUser
-        .save()
-        .then(user => {
-          res.redirect('/')                   // 新增完成導回首頁
+
+      bcrypt.genSalt(10, (err, salt) =>
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          newUser.password = hash
+
+          newUser
+            .save()
+            .then(user => {
+              res.redirect('/')                   // 新增完成導回首頁
+            })
+            .catch(err => console.log(err))
         })
-        .catch(err => console.log(err))
+      )
+
     }
   })
 })
+
 // 登出
 router.get('/logout', (req, res) => {
   req.logout()
